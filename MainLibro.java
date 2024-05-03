3e3f81f55b8401e143f6866ce2f0ef667aab1623
Aldo Isidro <aldoisidro0@gamil.com>
2024-05-03 01:17:53 -0600

    version 1

---
+++ MainLibro.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MainLibro {
+    public static void main(String[] args) {
+        Libro librito = new Libro();
+        String a, e;
+        int i, o;
+
+        Scanner op = new Scanner(System.in);
+        System.out.print("Ingresa el Titulo del libro: ");
+        a = op.nextLine();
+        System.out.print("Ingresa el nombre del Autor: ");
+        e = op.nextLine();
+        System.out.print("Ingresa el ISBN: ");
+        i = op.nextInt();
+        System.out.print("Ingresa El numero de paginas: ");
+        o = op.nextInt();
+
+        librito.titulo = a;
+        librito.autor = e;
+        librito.isbn = i;
+        librito.numpag = o;
+
+        librito.MostrarTitulo();
+        librito.MostarAutor();
+        librito.MostrarISBN();
+        librito.MostarNumeroDePaginas();
+        Libro libro1 = new Libro("locos", "aldo", 14, 40);
+
+    }
+}
